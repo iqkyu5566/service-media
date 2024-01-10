2f67c929a0a1c313ad7f8cbc7da32924477948b7
Ilham J.E <ilham5566@gmail.com>
2024-01-10 13:36:03 +0800

    service-media

---
+++ models/media.js
@@ -0,0 +1,31 @@
+module.exports = (sequelize, DataTypes) => {
+  const Media = sequelize.define(
+    "Media",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+    },
+
+    {
+      tableName: "media",
+    }
+  );
+
+  return Media;
+};
